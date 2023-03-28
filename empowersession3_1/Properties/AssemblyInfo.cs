d704bcba4ebb2e94de1dc8db057534365f1620a1
jpb <jpb@BGOLAP-JPB>
2023-03-28 16:19:05 +0200

    Add project files.

---
+++ empowersession3_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("empowersession3_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("empowersession3_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E41B7639-EE5A-4FEB-9A75-A1880D9539FC")]
+[assembly: AssemblyVersion("1.0.0.0")]
